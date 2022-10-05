2e4b61676e5afcf4352994ea0f68069a66e89266
Ekundayo John-Tobi <johntobismart@gmail.com>
2022-10-05 22:44:09 +0100

    added new files

--- pro.js
+++ pro.js
@@ -3,8 +3,55 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
+    header.style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-100px";
+    header.style.top = "-150px";
   }
 }
+
+// Active Change
+let header = document.getElementById("navbar")
+let btns= header.getElementsByClassName("btn")
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active"
+  });
+  
+}
+
+
+
+ // Nav Link
+
+ let homes = document.getElementById("homes")
+ let service = document.getElementById("service")
+ let abouts = document.getElementById("abouts")
+ let contacts = document.getElementById("contacts")
+
+ homes.addEventListener("click", function () {
+  location.href = "#home"
+ })
+
+ service.addEventListener("click", function () {
+  location.href = "#services"
+ })
+
+ abouts.addEventListener("click", function () {
+  location.href = "#about"
+ })
+
+
+ contacts.addEventListener("click", function () {
+  location.href = "#contact"
+ })
+
+ let btnss= document.getElementById("button")
+
+ btnss.addEventListener("click", function () {
+  location.href= "#about"
+ })
+
+
